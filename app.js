0f7ff46bd9f0e0b714b8cd6d2fcdf9327c489f5b
Mark Estrada <h.mark.estrada@gmail.com>
2022-05-31 19:33:02 -0500

    Updated Testing
    Updated Testing

--- app.js
+++ app.js
@@ -6,33 +6,33 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/node-demo");
-var nameSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    eventTitle: String,
-    eventDate: String
-});
-var User = mongoose.model("User", nameSchema);
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://ourglass:test1234@cluster0.wkxpvy0.mongodb.net/?retryWrites=true&w=majority";
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
-
-app.post("/addname", (req, res) => {
-    var myData = new User(req.body);
-    myData.save()
-        .then(item => {
-            res.send("Test name created and saved to MongoDB");
-        })
-        .catch(err => {
-            res.status(400).send("Unable to save to database");
-        });
-});
-
-app.listen(port, () => {
-    console.log("Server listening on port " + port);
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("our-glass-test");
+  var myobj = [
+    { name: 'Brandonita', address: 'Highway 71'},
+    { name: 'Chuck', address: 'Main Road 989'},
+    { name: 'John', address: 'Highway 71'},
+    { name: 'Peter', address: 'Lowstreet 4'},
+    { name: 'Amy', address: 'Apple st 652'},
+    { name: 'Hannah', address: 'Mountain 21'},
+    { name: 'Michael', address: 'Valley 345'},
+    { name: 'Sandy', address: 'Ocean blvd 2'},
+    { name: 'Betty', address: 'Green Grass 1'},
+    { name: 'Richard', address: 'Sky st 331'},
+    { name: 'Susan', address: 'One way 98'},
+    { name: 'Vicky', address: 'Yellow Garden 2'},
+    { name: 'Ben', address: 'Park Lane 38'},
+    { name: 'William', address: 'Central st 954'},
+    { name: 'Chuck', address: 'Main Road 989'},
+    { name: 'Viola', address: 'Sideway 1633'}
+  ];
+  dbo.collection("User").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
 });
